d4608ad2779ad1bbc98faf34ab0dd37566a44290
Mashnar <szymon.fr@gmail.com>
2019-01-07 13:50:41 +0100

    Testowy

--- src/main/java/LibraryProject/Library/DB/CRUD/BooksRepository.java
+++ src/main/java/LibraryProject/Library/DB/CRUD/BooksRepository.java
@@ -6,8 +6,13 @@ package LibraryProject.Library.DB.CRUD;
 // CRUD refers Create, Read, Update, Delete
 
 import LibraryProject.Library.DB.Books;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface BooksRepository extends CrudRepository<Books, Integer> {
+@Query("SELECT * FROM USER");
+Set<Books>temp getId();
 
 }
